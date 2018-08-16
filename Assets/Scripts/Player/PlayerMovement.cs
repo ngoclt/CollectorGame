5f08603c73980ec3cdb7a0abd6a3905eb1382fb6
Ngoc LE <ngoc.le@smarp.com>
2018-08-16 23:08:38 +0300

    Added animator controller for Archer Player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    bool jump = false;
+    bool shot = false;
+
+    float horizontalMove = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+	}
+}
